fcca5cce9ff99d30461c61eec9ac48fa6ad7b1c5
Gagan <gagan2@umbc.edu>
2012-05-06 15:23:15 -0400

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,16 @@ attr_accessor :password
 attr_accessible :name, :email, :password, :password_confirmation
 
 has_many :microposts, :dependent => :destroy
+has_many :relationships, :foreign_key => "follower_id",
+:dependent => :destroy
+has_many :reverse_relationships, :foreign_key => "followed_id",
+:class_name => "Relationship",
+:dependent => :destroy
+has_many :followers, :through => :reverse_relationships, :source => :follower
+has_many :following, :through => :relationships, :source => :followed
 
-
+scope :admin, where(:admin => true)
+	
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :name, :presence => true,
 :length => { :maximum => 50 }
@@ -21,9 +29,9 @@ validates :password, :presence => true,
 before_save :encrypt_password
 
 def feed
-# This is preliminary. See Chapter 12 for the full implementation.
-Micropost.where("user_id = ?", id)
+Micropost.from_users_followed_by(self)
 end
+
 
 def has_password?(submitted_password)
 # Compare encrypted_password with the encrypted version of
@@ -36,6 +44,18 @@ def self.authenticate(email, submitted_password)
 user = find_by_email(email)
 return nil if user.nil?
 return user if user.has_password?(submitted_password)
+end
+
+
+
+def following?(followed)
+relationships.find_by_followed_id(followed)
+end
+def follow!(followed)
+relationships.create!(:followed_id => followed.id)
+end
+def unfollow!(followed)
+relationships.find_by_followed_id(followed).destroy
 end
 
 
